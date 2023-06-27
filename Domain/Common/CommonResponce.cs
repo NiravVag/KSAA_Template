9d09adf126fc167e4a1b4bcd70a2f9ed224edaf1
Nirav Vaghela <89123791+NiravVag@users.noreply.github.com>
2023-06-27 19:24:11 +0530

    Commit_Init

---
+++ Domain/Common/CommonResponce.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public abstract class CommonResponce<T>
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public dynamic? Response { get; set; }
+    }
+}
---
+++ Domain/Entity/GL_IncomeModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class GL_IncomeModel
+    {
+        public List<GL_IncomeData>? GL_IncomeData { get; set; }
+        public decimal Sum_GL { get; set; }
+        public decimal Sum_supply { get; set; }
+        public decimal diff { get; set; }
+        public List<SupplyModel>? SupplyModels { get; set; }
+    }
+
+    public class GL_IncomeData
+    {
+        public int Income_Booking_Resp_GL_ID { get; set; }
+        public string? GL_Code { get; set; }
+        public string? GL_Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    public class SupplyModel
+    {
+        public string? GL_Code { get; set; }
+        public string? Accounting_voucher { get; set; }
+        public string? Amount { get; set; }
+        public string? Description { get; set; }
+        public string? Date { get; set; }
+        public string? HSN_SAC { get; set; }
+        public string? CGST { get; set; }
+        public string? SGST { get; set; }
+        public string? IGST { get; set; }
+        public string? CESS { get; set; }
+        public string? GSTIN { get; set; }
+        public string? Remarks { get; set; }
+        public string? Action { get; set; }
+
+    }
+}
